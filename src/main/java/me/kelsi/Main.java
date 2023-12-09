bb1ce7023020d9574b6139dfd45be0272d060dd3
Kelsi <superigromen2018@gmail.com>
2023-12-09 12:54:13 +0300

    develop

--- src/main/java/me/kelsi/Main.java
+++ src/main/java/me/kelsi/Main.java
@@ -3,12 +3,19 @@ package me.kelsi;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-
-        String token = "OTg0Nzk4OTk4OTExMTU2Mjg1.GSqmP2.uzGl3K0Ze70qvbt6KpXO_H1xGVZUH2uO4q5ktw";
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(new File("src/main/resources/token.txt"));
+        String token = sc.nextLine();
         JDA bot = JDABuilder.createDefault(token).setActivity(Activity.playing("Копается в шахте...")).build();
 
+
+        }
     }
 }
