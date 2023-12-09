a5f9458d44f3026b02c4356d7021b71193a039e0
Kelsi <superigromen2018@gmail.com>
2023-12-09 12:11:43 +0300

    develop

--- src/main/java/me/kelsi/Main.java
+++ src/main/java/me/kelsi/Main.java
@@ -1,7 +1,14 @@
 package me.kelsi;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String token = "OTg0Nzk4OTk4OTExMTU2Mjg1.GSqmP2.uzGl3K0Ze70qvbt6KpXO_H1xGVZUH2uO4q5ktw";
+        JDA bot = JDABuilder.createDefault(token).setActivity(Activity.playing("Копается в шахте...")).build();
+
     }
 }
