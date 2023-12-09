f6ad206928602fb5d5fcf4a100d01c45d7b1c70b
Kelsi <superigromen2018@gmail.com>
2023-12-09 16:37:10 +0300

    develop

--- src/main/java/me/kelsi/Listeners.java
+++ src/main/java/me/kelsi/Listeners.java
@@ -3,6 +3,8 @@ package me.kelsi;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 
 public class Listeners extends ListenerAdapter {
@@ -10,7 +12,7 @@ public class Listeners extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         Guild guild = event.getJDA().getGuildById(918438356642324480L);
-        guild.upsertCommand("profile", "Показывает ваш профиль в боте").queue();
+        guild.upsertCommand("profile", "Показывает профиль пользователя в боте").addOptions(new OptionData(OptionType.USER, "user", "Если хотите увидеть свой профиль, то ничего не пишите в этом аргументе", false)).queue();
     }
 
 }
--- src/main/java/me/kelsi/commands/profile.java
+++ src/main/java/me/kelsi/commands/profile.java
@@ -1,15 +1,42 @@
 package me.kelsi.commands;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public class profile extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (!event.getName().equalsIgnoreCase("profile")) return;
-        event.reply("Команда работает!").queue();
+        OptionMapping user = event.getOption("user");
+        User ds_user = null;
+        EmbedBuilder builder = new EmbedBuilder();
+        if (user != null) {
+            ds_user = user.getAsUser();
+        } else if (user == null) {
+            ds_user = event.getUser();
+        }
+        String username = ds_user.getName();
+
+
+        builder.setTitle("Профиль " + username);
+        builder.setColor(Color.PINK);
+        builder.setDescription("**Вскопано всего блоков:**\n%value% <:blocks_ms:1183029079780368464>");
+        builder.addField("**Типы блоков**", "типы блоков, которые вы добыли", false);
+        builder.addField("Вскопано камня: ", "%value% <:stone_ms:1182703138285826119>", true);
+        builder.addField("Вскопано угля: ", "%value% <:coal_ore_ms:1182711904045641838>", true);
+        builder.addField("Вскопано железа: ", "%value% <:iron_ore_ms:1182703752092844143>", true);
+        builder.addField("Вскопано золота: ", "%value% <:gold_ore_ms:1182710543333740545>", true);
+        builder.addField("Вскопано алмазов: ", "%value% <:diamond_ore_ms:1182711031110307860>", true);
+        builder.addField("Вскопано изумрудов: ", "%value% <:emerald_ore_ms:1182710781930909787>", true);
+        builder.addField(" ", " ", false);
+        event.replyEmbeds(builder.build()).queue();
     }
 
 }
